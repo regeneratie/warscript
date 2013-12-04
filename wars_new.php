5ed773a132a22ff2c0ad9c50fd0808a6e5317587
regeneratie <regeneratie@gmail.com>
2013-12-04 02:38:12 +0100

    remove session function for debug

--- wars_new.php
+++ wars_new.php
@@ -10,10 +10,7 @@
  {
  
 
-session_start();
-if (!isset($_SESSION['member_id'])){
-header('location:index.php');
-}
+
 //
  ?>
  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
