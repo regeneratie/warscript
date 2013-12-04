692e10c3b447efdc6791e9175c911494c424e86a
douwe <douwe@ubuntu.(none)>
2013-12-04 18:32:22 +0100

    update db

--- admin/dbconn.php
+++ admin/dbconn.php
@@ -7,6 +7,6 @@ mysql_connect("localhost","root","XXXXXXX"); //(host, username, password)
 
 //specify database
 
-mysql_select_db("XXXXXX") or die("Unable to select database"); //select which database we're using
+mysql_select_db("warscript") or die("Unable to select database"); //select which database we're using
 
 ?>
