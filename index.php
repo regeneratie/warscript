2bc820defee80b0d478437770fd90f525c58ab32
regeneratie <regeneratie@gmail.com>
2013-12-04 04:12:34 +0100

    add login

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
 		</head>
 <body>
 
+
+<div class="menu"> 
+<div class="datagrid"><table width="300" align="center">
+<thead><tr><th></th><th>Login</th></tr></thead>
+<form method="post" action="../admin/login.php"> 
+<tr><td>Username:</td><td><input type="text" name="UserName"/></td>
+<tr><td>Password:</td><td><input type="password" name="Password"/></td>
+<tr><td><input type="submit" name="submit" value="Login"/></td>
+</form>
+</div>
+</div>
+
+
 <?php
 
 include "wars.php";
