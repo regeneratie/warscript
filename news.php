cdff16ab5d703995478fb64aae4e1f0de5406454
regeneratie <regeneratie@gmail.com>
2013-12-04 19:16:12 +0100

    finaly got a working table of the news addon

--- news.php
+++ news.php
@@ -1,9 +1,4 @@
 <?php
-session_start();
-if (!isset($_SESSION['member_id'])){
-header('location:error.php');
-}
-//
 include('./admin/dbconn.php');
 
 $result = mysql_query("SELECT * FROM news ORDER BY id DESC") or die(mysql_error());
@@ -11,14 +6,14 @@ $result = mysql_query("SELECT * FROM news ORDER BY id DESC") or die(mysql_error(
 //Get info out of the database if in array
 while($row = mysql_fetch_array($result))
   {
-echo "<div class='datagrid'><table width='800' align='center'>";
-echo "<thead><tr><th>head</th>". $row['head'] . "</tr></thead>" ;
-echo "<td>";
-echo $row['date'];
-echo "</tr></td>";
-echo "<tr><td>";
-echo $row['text'];
-echo "</tr></td>";
+echo "<table width='800' align='center'>";
+
+echo "<td><b>" . $row[titel] . "</b></td>";
+echo "<tr>";
+echo "<td>" . $row[text] . "</td>";
+echo "<tr>";
+echo "<td>" . $row[date] . "</td>";
+echo "</table>";
   }
 
 
