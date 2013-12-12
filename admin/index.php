fa11a2fbf6de412f100a985def1ec3905c71eadb
regeneratie <regeneratie@gmail.com>
2013-12-12 17:15:23 +0100

    Oops typo there :S

--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,7 @@ header('location:./error.php');
 }
 
 
-echo "<br/><center><a href='logout.php'>logout</a>  <a href='News.php'>news</a>  <a href='wars.php'>Wars</a></center>";
+echo "<br/><center><a href='logout.php'>logout</a>  <a href='news.php'>News</a>  <a href='wars.php'>Wars</a></center>";
 
 include "./wars.php";
 
