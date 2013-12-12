f8bca1420afc717ac840e843ff368e762dd7f0c6
regeneratie <regeneratie@gmail.com>
2013-12-12 15:02:39 +0100

    fixxed bug in edit news

--- admin/news_edit.php
+++ admin/news_edit.php
@@ -10,7 +10,7 @@ header('location:error.php');
 
  // creates the edit record form
  // since this form is used multiple times in this file, I have made it a function that is easily reusable
- function renderForm($titel, $text, $date, $poster, $error)
+ function renderForm($id, $date, $titel, $text, $poster, $error)
  {
  ?>
  <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -36,10 +36,11 @@ header('location:error.php');
  <input type="hidden" name="id" value="<?php echo $id; ?>"/>
 <div class="datagrid"><table width="800" align="center">
 <thead><tr><th>Read</th><th>Input</th></tr></thead>
- <td><strong>titel</strong></td> <td><input type="text" name="titel" value="<?php echo $titel; ?>" /></td><tr><br/>
-</tr><td><strong>text</strong></td> <td><input type="text" name="text" value="<?php echo $text; ?>" /></td><tr>
- </tr><td><strong>date</strong></td> <td><input type="datetime-local" name="date" value="<?php echo $date; ?>" /></td><tr><br/>
- </tr><td><strong>poster</strong></td> <td><input type="text" name="poster" value="<?php echo $poster; ?>" /><tr><br/>
+ <td><strong>date</strong></td> <td><input type="text" name="date" value="<?php echo $date; ?>" /></td><tr>
+ </tr><td><strong>titel</strong></td> <td><input type="text" name="titel" value="<?php echo $titel; ?>" /></td><tr>
+ </tr><td><strong>text</strong></td> <td><input type="text" name="text" value="<?php echo $text; ?>" /></td><tr>
+ </tr><td><strong>poster</strong></td> <td><input type="text" name="poster" value="<?php echo $poster; ?>" /><tr>
+ </tr><td><p>* fubar score</p><tr></td>
   </tr><td><input type="submit" name="submit" value="Submit"><td>
  </table>
  </div>
@@ -62,35 +63,35 @@ header('location:error.php');
  {
  // get form data, making sure it is valid
  $id = $_POST['id'];
+ $date = mysql_real_escape_string(htmlspecialchars($_POST['date']));
  $titel = mysql_real_escape_string(htmlspecialchars($_POST['titel']));
  $text = mysql_real_escape_string(htmlspecialchars($_POST['text']));
- $date = mysql_real_escape_string(htmlspecialchars($_POST['date']));
  $poster = mysql_real_escape_string(htmlspecialchars($_POST['poster']));
+
  
  // check that firstname/lastname fields are both filled in
- if ($titel == '' || $text == '' || $date == '' || $poster == '')
+ if ($date == '' || $titel == '' || $text == '' || $poster == '')
  {
  // generate error message
  $error = 'ERROR: Please fill in all required fields!';
  
  //error, display form
- renderForm($id, $titel, $text, $date, $poster, $error);
+ renderForm($id, $date, $titel, $text, $poster, $error);
  }
  else
  {
  // save the data to the database
- mysql_query("UPDATE news SET titel='$titel', text='$text', date='$date', poster='$poster' WHERE id='$id'")
+ mysql_query("UPDATE news SET date='$date', titel='$titel', text='$text', poster='$poster' WHERE id='$id'")
  or die(mysql_error()); 
  
  // once saved, redirect back to the view page
  header("Location: ../wars.php"); 
  }
- echo "lol";
  }
  else
  {
  // if the 'id' isn't valid, display an error
- echo 'Error! 1234';
+ echo 'Error!';
  }
  }
  else
@@ -112,15 +113,15 @@ header('location:error.php');
  
  // get data from db
 
+ $date = $row['date'];
  $titel = $row['titel'];
  $text = $row['text'];
- $date = $row['date'];
  $poster = $row['poster'];
 
  
  
  // show form
- renderForm($titel, $text, $date, $poster, '');
+ renderForm($id ,$date, $titel, $text, $poster, '');
  }
  else
  // if no match, display result
