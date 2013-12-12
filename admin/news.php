144f4c3e3a1e81f327f91b3603aceca513d4cd09
regeneratie <regeneratie@gmail.com>
2013-12-12 16:18:01 +0100

    clean up

---
+++ admin/news.php
@@ -0,0 +1,28 @@
+<?php
+include('./admin/dbconn.php');
+
+$result = mysql_query("SELECT * FROM news ORDER BY id DESC") or die(mysql_error());
+
+//Get info out of the database if in array
+while($row = mysql_fetch_array($result))
+  {
+echo "<table width='800' align='center'>";
+
+echo "<td><b>" . $row[titel] . "</b></td>";
+echo "<tr>";
+echo "<td>" . $row[text] . "</td>";
+echo "<tr>";
+echo "<td>" . $row[date] . "</b></td>";
+echo "<tr>";
+echo "<td>Posted by <b>" . $row[poster] . '</b>  ' . $row[date] ."</td>";
+echo "<tr>";
+echo '<td><a href="./admin/news_edit.php?id=' . $row['id'] . '"><img src="./pics/edit.png" />
+		  <a href="./admin/news_delete.php?id=' . $row['id'] . '"><img src="./pics/delete.png" />
+		  <a href="./admin/news_add.php"><img src="./pics/add.png" />
+		  </a></td>';
+echo "</table>";
+  }
+
+
+mysql_close;
+?>
