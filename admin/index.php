82a6318ee3ae6d584ffef313581f97801fa5964c
regeneratie <regeneratie@gmail.com>
2013-12-24 19:35:37 +0100

    add working menu

--- admin/index.php
+++ admin/index.php
@@ -3,15 +3,26 @@ session_start();
 if (!isset($_SESSION['member_id'])){
 header('location:./error.php');
 }
+?>
+
+<center>
+<a href="index.php?page=news">News</a>
+<a href="index.php?page=wars">Wars</a>
+<a href="index.php?page=member">Members</a>
+<a href="index.php?page=logout">Logout</a>
+</center>
+
+<?php
 
 
-echo "<br/><center><a href='logout.php'>logout</a>  <a href='news.php'>News</a>  <a href='wars.php'>Wars</a></center>";
-
-include "./wars.php";
-
-include "./news.php";
-
-
+	$page = $_GET['page'];
+	if (!empty($page)) {
+		$page .= '.php';
+		include($page);
+	}
+	else {
+		include('news.php');
+	}
 
 
 ?>
