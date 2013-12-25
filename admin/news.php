d8960da3841658f0f603f1edce646db0e830e8a8
regeneratie <regeneratie@gmail.com>
2013-12-25 22:50:43 +0100

    add visable option to news script

--- admin/news.php
+++ admin/news.php
@@ -1,7 +1,11 @@
 <?php
+session_start();
+if (!isset($_SESSION['member_id'])){
+header('location:error.php');
+}
 include('./dbconn.php');
 
-$result = mysql_query("SELECT * FROM news ORDER BY id DESC") or die(mysql_error());
+$result = mysql_query("SELECT * FROM news WHERE `delete` = 'no' ORDER BY id DESC") or die(mysql_error());
 
 //Get info out of the database if in array
 while($row = mysql_fetch_array($result))
@@ -15,8 +19,9 @@ echo "<tr>";
 echo "<td><br>Posted by <b>" . $row[poster] . '</b>  ' . $row[date] ."</td>";
 echo "<tr>";
 echo '<td><br><a href="./news_edit.php?id=' . $row['id'] . '"><img src="../pics/edit.png" />
-		  <a href="./news_delete.php?id=' . $row['id'] . '"><img src="../pics/delete.png" />
 		  <a href="./news_add.php"><img src="../pics/add.png" />
+		  <a href="./news_visable.php?id=' . $row['id'] . '"><img src="../pics/visable.png" />
+		  <a href="./news_delete.php?id=' . $row['id'] . '"><img src="../pics/delete.png" />
 		  </a></td>';
 echo "</table>";
   }
