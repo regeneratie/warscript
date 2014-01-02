72f3cf1d52f2538613a7ab9e4a2fbd9546a5ea06
regeneratie <regeneratie@gmail.com>
2014-01-02 02:06:13 +0100

    lower case

--- admin/index.php
+++ admin/index.php
@@ -47,7 +47,7 @@ header('location:./error.php');
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">News<b class="caret"></b></a>
 					<ul class="dropdown-menu">
 					  <li><a href="index.php?page=news">View <img align="right" src="../pics/view.png" /></a></li>
-					  <li><a href="index.php?page=news_add">ADD <img align="right" src="../pics/add.png" /></a></li>
+					  <li><a href="index.php?page=news_add">Add <img align="right" src="../pics/add.png" /></a></li>
 					  <li><a href="index.php?page=news_trash">Trash <img align="right" src="../pics/visable.png" /></a></li>
 					</ul>
 				</li>
@@ -55,7 +55,7 @@ header('location:./error.php');
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Wars<b class="caret"></b></a>
 					<ul class="dropdown-menu">
 					  <li><a href="index.php?page=wars">View <img align="right" src="../pics/view.png" /></a></li>
-					  <li><a href="index.php?page=wars_add">ADD <img align="right" src="../pics/add.png" /></a></li>
+					  <li><a href="index.php?page=wars_add">Add <img align="right" src="../pics/add.png" /></a></li>
 					  <li><a href="index.php?page=wars_trash">Trash <img align="right" src="../pics/visable.png" /></a></li>
 					</ul>
 				</li>
@@ -63,7 +63,7 @@ header('location:./error.php');
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Members<b class="caret"></b></a>
 					<ul class="dropdown-menu">
 					  <li><a href="index.php?page=members">View <img align="right" src="../pics/view.png" /></a></li>
-					  <li><a href="index.php?page=members_add">ADD <img align="right" src="../pics/add.png" /></a></li>
+					  <li><a href="index.php?page=members_add">Add <img align="right" src="../pics/add.png" /></a></li>
 					  <li><a href="index.php?page=members_trash">Trash <img align="right" src="../pics/visable.png" /></a></li>
 					</ul>
 				</li>
