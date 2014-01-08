41f828d17c98f200235484a64746fd78b4799151
regeneratie <regeneratie@gmail.com>
2014-01-08 02:17:48 +0100

    edit clans

--- admin/wars/wars_clans.php
+++ admin/wars/wars_clans.php
@@ -6,7 +6,7 @@ include('dbconn.php');
 function enemy_select()
  {
 //Select data from database
-$enemy = mysql_query("SELECT * FROM enemy ORDER BY id DESC") or die(mysql_error());
+$enemy = mysql_query("SELECT * FROM enemy WHERE `delete` = 'no' ORDER BY id DESC") or die(mysql_error());
 
   echo "<select name=clan>"; 
  while($row = mysql_fetch_array($enemy))
